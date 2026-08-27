975a84bbb2aa18f1df71b8d521a9307dd0408d35
Andrey <andreyra00@gmail.com>
2026-08-27 12:45:47 +0300

    rust setup

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -184,8 +184,16 @@ require("lazy").setup({
                 { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
             },
         },
-        { "catppuccin/nvim",       name = "catppuccin", priority = 1000 }
-    },
+        { "catppuccin/nvim",       name = "catppuccin", priority = 1000 },
+        {
+            'mrcjkb/rustaceanvim',
+            -- To avoid being surprised by breaking changes,
+            -- I recommend you set a version range
+            version = '^9',
+            -- This plugin implements proper lazy-loading (see :h lua-plugin-lazy).
+            -- No need for lazy.nvim to lazy-load it.
+            lazy = false,
+        } },
     -- automatically check for plugin updates
     checker = { enabled = true },
 })
